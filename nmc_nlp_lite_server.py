2c6ba9b65f4343fa9700a54d762488daf7dc1803
nmc dev <jjdev@nmcomputing.com>
2020-05-24 16:28:30 -0700

    Rebase from 'upstream'

--- nmc_nlp_lite_server.py
+++ nmc_nlp_lite_server.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python
 
-#2020.5.13 this is the nmc_nlp_lite ROS package main file.
-import roslib  #for listner
-import sys  #for listener 
+#2020.5.13 This is the nmc_nlp_lite ROS package main file.
+#2020.5.13 Its main function: a ROS node that accesses a cloud-based light-weight natural language normalization service, which is hosted at www.nmcomputing.com
+#  AS a ROS node it can be accessed through a service call, or through publish/subscribe mode.
+
 import rospy #for talker & listner 
-from std_msgs.msg import String #for talker & listner 
 from nmc_nlp_lite.srv import nmcNLP,nmcNLPResponse
+from nmc_nlp_lite.msg import nmcNLPMsg
+import nmc_bins as nmcbins
 
-import nmcbins
+pub = rospy.Publisher('nmc_nlp_out', nmcNLPMsg, queue_size=10)
 
-pub = rospy.Publisher('nmc_nlp_out', String, queue_size=10)
-rospy.init_node('nmc_nlp', anonymous=True)  # 'nmc_nlp' as node name is overwritten by the name specified in nmc_nlp_lite.launch file.
-nmcbins.setQid("p7INVIHYDQM")
-text_to_process = 'a male b'
+def callback_for_subscriber(bins_input):
+	msg = rospy.get_caller_id() + "  relay relay relay" + bins_input.binsSentence
+	rospy.loginfo(msg)
+	nmcbins.setQid(bins_input.binsClientQid)
+	bins_response=nmcbins.normalize(bins_input.binsSentence)
+	bins_output = nmcNLPMsg()
+	bins_output.binsClientQid= bins_input.binsClientQid
+	bins_output.binsSentence= bins_response
+	pub.publish(bins_output)
 
-def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + "  nmc_nlp server received: %s", data.data)
-	b=nmcbins.normalize(text_to_process)
-	rospy.loginfo(b)
-	msg = rospy.get_caller_id() + "  relay relay relay" + data.data
-	pub.publish(msg)
-
-def serviceCallback(req):
-	b=nmcbins.normalize(req.str)
-	return nmcNLPResponse("req:"+req.str + "\n response:" + b)
+def callback_for_service(req):
+	nmcbins.setQid(req.binsClientQid)
+	bins_response=nmcbins.normalize(req.binsInputSentence)
+	return nmcNLPResponse("bins_input:"+req.binsInputSentence + "\n bins_output:" + bins_response)
 
 def listener():
-    #rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("nmc_nlp_in", String, callback)
-    rospy.Service("nmc_nlp_service",nmcNLP, serviceCallback)
-    # spin() simply keeps python from exiting until this node is stopped
+    rospy.init_node('nmc_nlp', anonymous=True)  # 'nmc_nlp' as node name is overwritten by the name specified in nmc_nlp_lite.launch file.	
+    rospy.Subscriber("nmc_nlp_in", nmcNLPMsg, callback_for_subscriber)
+    rospy.Service("nmc_nlp_service",nmcNLP, callback_for_service)
     rospy.spin()
 
 if __name__ == '__main__':
